14c8866a8751616e0e6cbf4ad6b15103e98d93be
Marvin Louie Fabre <marvinlouiefabre@gmail.com>
2022-09-11 19:52:52 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let coin = document.querySelector(".coin");
+let btnFlip = document.querySelector("#flip-button");
+let btnReset = document.querySelector("#reset-button");
+let heads = 0;
+let tails = 0;
